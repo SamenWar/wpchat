1cd20139a6a5ec9c416b37ebc3a8fac644e85171
SamenWar <SamenWarrior@gmail.com>
2023-05-15 16:51:23 +0200

    iframe

--- components/Vidio.tsx
+++ components/Vidio.tsx
@@ -6,11 +6,23 @@ interface VideoProps {
 
 const Video: React.FC<VideoProps> = ({ videoId }) => {
     return (
-        <div className="content">
+        <div
+            className="content"
+            style={{
+                backgroundImage: `url('img/image1.png')`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+            }}
+        >
             <iframe
                 src={`https://www.youtube.com/embed/${videoId}`}
                 frameBorder="0"
-                allowFullScreen>
+                allowFullScreen
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
             </iframe>
         </div>
     );
